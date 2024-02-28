9b88b362740fdd10da2ae6b9bd6b1d77dd0912f5
Wei-Yuan Hsing <bfyuan0111@gmail.com>
2024-02-28 03:25:08 -0800

    Base data type added

---
+++ Source/TD1/Public/TD1GameStateBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "TD1GameStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TD1_API ATD1GameStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
